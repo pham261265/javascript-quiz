cb87f837a02ced47a9312e97ae14440160f86492
niku senpai <pbpham97@gmail.com>
2020-08-23 15:23:30 -0500

    start button

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 const startButton = document.getElementById('start-btn')
-const questionContainerElement = document.getElementById
-('question-container')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
 
 function startGame() {
     console.log('started')
     startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
     questionContainerElement.classList.remove('hide')
     setNextQuestion()
 }
 
 function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
 
 }
 
@@ -19,3 +30,12 @@ function selectAnswer() {
 
 }
 
+const questions = [
+    {
+        question: 'Which one of these is a JavaScript package manager?',
+        answers: [
+            { text: '4', correct: true},
+            { text: '22', correct: false},
+        ]
+    }
+]
