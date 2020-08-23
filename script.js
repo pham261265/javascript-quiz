bc00ee84589912fff63b8abc3856b64312d01a65
niku senpai <pbpham97@gmail.com>
2020-08-23 14:55:55 -0500

    styles

---
+++ script.js
@@ -0,0 +1,21 @@
+const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById
+('question-container')
+
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+    console.log('started')
+    startButton.classList.add('hide')
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
+}
+
+function setNextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
+
